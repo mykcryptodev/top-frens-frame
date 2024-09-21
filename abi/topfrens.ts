c620b1a062ebb7877245bf39a01c7e985513732d
Mike Solomon <mike.solomon@coinbase.com>
2024-09-21 15:31:54 -0400

    initial commit

---
+++ abi/topfrens.ts
@@ -0,0 +1,515 @@
+export const abi = [
+  {
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "_verificationsV4Reader",
+        "type": "address"
+      }
+    ],
+    "stateMutability": "nonpayable",
+    "type": "constructor"
+  },
+  {
+    "inputs": [],
+    "name": "MaxTopFrensReached",
+    "type": "error"
+  },
+  {
+    "inputs": [],
+    "name": "NotAuthorized",
+    "type": "error"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "owner",
+        "type": "address"
+      }
+    ],
+    "name": "OwnableInvalidOwner",
+    "type": "error"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "account",
+        "type": "address"
+      }
+    ],
+    "name": "OwnableUnauthorizedAccount",
+    "type": "error"
+  },
+  {
+    "anonymous": false,
+    "inputs": [
+      {
+        "indexed": true,
+        "internalType": "address",
+        "name": "previousOwner",
+        "type": "address"
+      },
+      {
+        "indexed": true,
+        "internalType": "address",
+        "name": "newOwner",
+        "type": "address"
+      }
+    ],
+    "name": "OwnershipTransferred",
+    "type": "event"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "user",
+        "type": "address"
+      },
+      {
+        "internalType": "address",
+        "name": "topFren",
+        "type": "address"
+      }
+    ],
+    "name": "addTopFrenByAddress",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "user",
+        "type": "address"
+      },
+      {
+        "internalType": "uint256",
+        "name": "topFrenFid",
+        "type": "uint256"
+      }
+    ],
+    "name": "addTopFrenByAddressAndFid",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "uint256",
+        "name": "userFid",
+        "type": "uint256"
+      },
+      {
+        "internalType": "uint256",
+        "name": "topFrenFid",
+        "type": "uint256"
+      }
+    ],
+    "name": "addTopFrenByFid",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "uint256",
+        "name": "userFid",
+        "type": "uint256"
+      },
+      {
+        "internalType": "address",
+        "name": "topFren",
+        "type": "address"
+      }
+    ],
+    "name": "addTopFrenByFidAndAddress",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "user",
+        "type": "address"
+      },
+      {
+        "internalType": "address[]",
+        "name": "topFrensToAdd",
+        "type": "address[]"
+      }
+    ],
+    "name": "addTopFrensByAddress",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "uint256",
+        "name": "userFid",
+        "type": "uint256"
+      },
+      {
+        "internalType": "uint256[]",
+        "name": "topFrenFids",
+        "type": "uint256[]"
+      }
+    ],
+    "name": "addTopFrensByFid",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "verifier",
+        "type": "address"
+      }
+    ],
+    "name": "getFid",
+    "outputs": [
+      {
+        "internalType": "uint256",
+        "name": "fid",
+        "type": "uint256"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "verifier",
+        "type": "address"
+      }
+    ],
+    "name": "getFidWithEvent",
+    "outputs": [
+      {
+        "internalType": "uint256",
+        "name": "fid",
+        "type": "uint256"
+      }
+    ],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "address[]",
+        "name": "verifiers",
+        "type": "address[]"
+      }
+    ],
+    "name": "getFids",
+    "outputs": [
+      {
+        "internalType": "uint256[]",
+        "name": "fid",
+        "type": "uint256[]"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "user",
+        "type": "address"
+      }
+    ],
+    "name": "getTopFrensFidsByAddress",
+    "outputs": [
+      {
+        "internalType": "uint256[]",
+        "name": "",
+        "type": "uint256[]"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "uint256",
+        "name": "fid",
+        "type": "uint256"
+      }
+    ],
+    "name": "getTopFrensFidsByFid",
+    "outputs": [
+      {
+        "internalType": "uint256[]",
+        "name": "",
+        "type": "uint256[]"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "maxTopFrens",
+    "outputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "owner",
+    "outputs": [
+      {
+        "internalType": "address",
+        "name": "",
+        "type": "address"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "user",
+        "type": "address"
+      }
+    ],
+    "name": "removeAllTopFrensByAddress",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "uint256",
+        "name": "userFid",
+        "type": "uint256"
+      }
+    ],
+    "name": "removeAllTopFrensByFid",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "user",
+        "type": "address"
+      },
+      {
+        "internalType": "address",
+        "name": "topFren",
+        "type": "address"
+      }
+    ],
+    "name": "removeTopFrenByAddress",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "user",
+        "type": "address"
+      },
+      {
+        "internalType": "uint256",
+        "name": "topFrenFid",
+        "type": "uint256"
+      }
+    ],
+    "name": "removeTopFrenByAddressAndFid",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "uint256",
+        "name": "userFid",
+        "type": "uint256"
+      },
+      {
+        "internalType": "uint256",
+        "name": "topFrenFid",
+        "type": "uint256"
+      }
+    ],
+    "name": "removeTopFrenByFid",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "uint256",
+        "name": "userFid",
+        "type": "uint256"
+      },
+      {
+        "internalType": "address",
+        "name": "topFren",
+        "type": "address"
+      }
+    ],
+    "name": "removeTopFrenByFidAndAddress",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "user",
+        "type": "address"
+      },
+      {
+        "internalType": "address[]",
+        "name": "topFrensToRemove",
+        "type": "address[]"
+      }
+    ],
+    "name": "removeTopFrensByAddress",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "user",
+        "type": "address"
+      },
+      {
+        "internalType": "uint256[]",
+        "name": "topFrenFids",
+        "type": "uint256[]"
+      }
+    ],
+    "name": "removeTopFrensByAddressAndFids",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "uint256",
+        "name": "userFid",
+        "type": "uint256"
+      },
+      {
+        "internalType": "uint256[]",
+        "name": "topFrenFids",
+        "type": "uint256[]"
+      }
+    ],
+    "name": "removeTopFrensByFid",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "renounceOwnership",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "uint256",
+        "name": "_maxTopFrens",
+        "type": "uint256"
+      }
+    ],
+    "name": "setMaxTopFrens",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "uint256",
+        "name": "fid",
+        "type": "uint256"
+      },
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "name": "topFrens",
+    "outputs": [
+      {
+        "internalType": "uint256",
+        "name": "topFrenFids",
+        "type": "uint256"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "newOwner",
+        "type": "address"
+      }
+    ],
+    "name": "transferOwnership",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "verificationsV4Reader",
+    "outputs": [
+      {
+        "internalType": "contract IVerificationsV4Reader",
+        "name": "",
+        "type": "address"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  }
+] as const;
--- api/index.tsx
+++ api/index.tsx
@@ -1,38 +1,49 @@
 import { Button, Frog, TextInput } from 'frog'
 import { devtools } from 'frog/dev'
 import { serveStatic } from 'frog/serve-static'
-// import { neynar } from 'frog/hubs'
+import { neynar } from 'frog/hubs'
 import { handle } from 'frog/vercel'
+import { createPublicClient, http } from 'viem'
+import { baseSepolia } from 'viem/chains'
+import { NeynarAPIClient } from "@neynar/nodejs-sdk";
+import dotenv from 'dotenv';
+dotenv.config();
 
-// Uncomment to use Edge Runtime.
-// export const config = {
-//   runtime: 'edge',
-// }
+// Assuming you have an ABI for the TopFrens contract
+import { abi } from '../abi/topfrens.js'
+
+// Replace with your actual contract address and RPC URL
+const CONTRACT_ADDRESS = '0x63b763765d25f3d25f3652303f2624b2c3ef3e5f' as const
+// const RPC_URL = 'https://...'
+
+const viemClient = createPublicClient({
+  chain: baseSepolia,
+  transport: http()
+})
+
+const client = new NeynarAPIClient(process.env.NEYNAR_API_KEY!);
+
 
 export const app = new Frog({
   assetsPath: '/',
   basePath: '/api',
   // Supply a Hub to enable frame verification.
-  // hub: neynar({ apiKey: 'NEYNAR_FROG_FM' })
-  title: 'Frog Frame',
+  hub: neynar({ apiKey: process.env.NEYNAR_API_KEY! }),
+  title: 'Top Frens',
 })
 
 app.frame('/', (c) => {
   const { buttonValue, inputText, status } = c
-  const fruit = inputText || buttonValue
+  const fid = inputText || buttonValue
+
   return c.res({
     image: (
       <div
         style={{
           alignItems: 'center',
-          background:
-            status === 'response'
-              ? 'linear-gradient(to right, #432889, #17101F)'
-              : 'black',
-          backgroundSize: '100% 100%',
+          background: 'linear-gradient(to right, #432889, #17101F)',
           display: 'flex',
           flexDirection: 'column',
-          flexWrap: 'nowrap',
           height: '100%',
           justifyContent: 'center',
           textAlign: 'center',
@@ -49,25 +60,226 @@ app.frame('/', (c) => {
             marginTop: 30,
             padding: '0 120px',
             whiteSpace: 'pre-wrap',
+            display: 'flex',
           }}
         >
           {status === 'response'
-            ? `Nice choice.${fruit ? ` ${fruit.toUpperCase()}!!` : ''}`
-            : 'Welcome!'}
+            ? `Viewing Top Frens for FID: ${fid}`
+            : 'Enter an FID to view Top Frens'}
         </div>
       </div>
     ),
     intents: [
-      <TextInput placeholder="Enter custom fruit..." />,
-      <Button value="apples">Apples</Button>,
-      <Button value="oranges">Oranges</Button>,
-      <Button value="bananas">Bananas</Button>,
-      status === 'response' && <Button.Reset>Reset</Button.Reset>,
+      <TextInput placeholder="Enter FID..." />,
+      <Button value="View">View Top Frens</Button>,
     ],
   })
 })
 
-// @ts-ignore
+app.frame('/top-frens', async (c) => {
+  const fid = c.inputText
+
+  if (!fid || isNaN(Number(fid))) {
+    return c.res({
+      image: (
+        <div style={{ display: "flex" }}>
+          <div style={{ display: "flex", flexDirection: "column" }}>
+            Invalid FID. Please enter a valid number.
+          </div>
+        </div>
+      ),
+      intents: [<Button.Reset>Go Back</Button.Reset>],
+    })
+  }
+
+  try {
+    const topFrenFids = await viemClient.readContract({
+      address: CONTRACT_ADDRESS,
+      abi,
+      functionName: 'getTopFrensFidsByFid',
+      args: [BigInt(fid)],
+    }) as bigint[]
+
+    console.log({ topFrenFids });
+
+    return c.res({
+      image: (
+        <div style={{ display: "flex" }}>
+          <div style={{ display: "flex", flexDirection: "column" }}>
+            Top Frens for FID {fid}:
+            {topFrenFids.map((frenFid, index) => (
+              <div style={{ display: "flex "}} key={index}>FID: {frenFid.toString()}</div>
+            ))}
+          </div>
+        </div>
+      ),
+      intents: [<Button.Reset>Search Another</Button.Reset>],
+    })
+  } catch (error) {
+    console.error('Error fetching top frens:', error)
+    return c.res({
+      image: (
+        <div style={{ display: "flex" }}>
+          <div style={{ display: "flex", flexDirection: "column" }}>
+            Error fetching top frens. Please try again.
+          </div>
+        </div>
+      ),
+      intents: [<Button.Reset>Try Again</Button.Reset>],
+    })
+  }
+})
+
+// New endpoint that takes FID from the URL
+app.frame('/fid/:fid', async (c) => {
+  const fid = c.req.param('fid')
+
+  if (!fid || isNaN(Number(fid))) {
+    return c.res({
+      image: (
+        <div style={{
+          alignItems: 'center',
+          background: 'linear-gradient(to right, #432889, #17101F)',
+          display: 'flex',
+          flexDirection: 'column',
+          height: '100%',
+          justifyContent: 'center',
+          textAlign: 'center',
+          width: '100%',
+        }}>
+          <div style={{
+            color: 'white',
+            fontSize: 40,
+            fontStyle: 'normal',
+            letterSpacing: '-0.025em',
+            lineHeight: 1.4,
+            marginTop: 30,
+            padding: '0 120px',
+            whiteSpace: 'pre-wrap',
+            display: 'flex',
+          }}>
+            Invalid FID. Please provide a valid number in the URL.
+          </div>
+        </div>
+      ),
+      intents: [
+        <Button action="/">Go to Home</Button>
+      ],
+    })
+  }
+
+  try {
+    const topFrenFids = await viemClient.readContract({
+      address: CONTRACT_ADDRESS,
+      abi,
+      functionName: 'getTopFrensFidsByFid',
+      args: [BigInt(fid)],
+    }) as bigint[]
+
+    const userFrens = await client.fetchBulkUsers(topFrenFids.map(Number));
+
+    return c.res({
+      image: (
+        <div style={{
+          alignItems: 'center',
+          background: 'linear-gradient(to right, #432889, #17101F)',
+          display: 'flex',
+          flexDirection: 'column',
+          height: '100%',
+          justifyContent: 'center',
+          textAlign: 'center',
+          width: '100%',
+          padding: '20px',
+        }}>
+          <div style={{
+            color: 'white',
+            fontSize: 32,
+            fontStyle: 'normal',
+            letterSpacing: '-0.025em',
+            lineHeight: 1.4,
+            marginTop: 30,
+            padding: '0 60px',
+            whiteSpace: 'pre-wrap',
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}>
+            Top Frens for FID {fid}:
+            <div style={{
+              display: 'flex',
+              flexWrap: 'wrap',
+              justifyContent: 'center',
+              gap: '20px',
+              marginTop: '20px',
+              maxWidth: '600px',
+            }}>
+              {userFrens.users.slice(0, 8).map((user, index) => (
+                <div key={index} style={{ 
+                  fontSize: 32, 
+                  display: 'flex', 
+                  alignItems: 'center', 
+                  gap: '10px',
+                  width: '280px',
+                  justifyContent: 'flex-start',
+                }}>
+                  <img 
+                    src={user.pfp_url} 
+                    alt={`${user.username}'s profile`} 
+                    style={{ 
+                      width: '72px', 
+                      height: '72px', 
+                      borderRadius: '50%' 
+                    }} 
+                  />
+                  <span style={{ overflow: 'hidden', textOverflow: 'ellipsis', whiteSpace: 'nowrap' }}>
+                    {user.username}
+                  </span>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      ),
+      intents: [
+        <Button action="/">Search Another</Button>
+      ],
+    })
+  } catch (error) {
+    console.error('Error fetching top frens:', error)
+    return c.res({
+      image: (
+        <div style={{
+          alignItems: 'center',
+          background: 'linear-gradient(to right, #432889, #17101F)',
+          display: 'flex',
+          flexDirection: 'column',
+          height: '100%',
+          justifyContent: 'center',
+          textAlign: 'center',
+          width: '100%',
+        }}>
+          <div style={{
+            color: 'white',
+            fontSize: 40,
+            fontStyle: 'normal',
+            letterSpacing: '-0.025em',
+            lineHeight: 1.4,
+            marginTop: 30,
+            padding: '0 120px',
+            whiteSpace: 'pre-wrap',
+            display: 'flex',
+          }}>
+            Error fetching top frens. Please try again.
+          </div>
+        </div>
+      ),
+      intents: [
+        <Button action="/">Go to Home</Button>
+      ],
+    })
+  }
+})
+
 const isEdgeFunction = typeof EdgeFunction !== 'undefined'
 const isProduction = isEdgeFunction || import.meta.env?.MODE !== 'development'
 devtools(app, isProduction ? { assetsPath: '/.frog' } : { serveStatic })
